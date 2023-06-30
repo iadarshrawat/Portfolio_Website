3c64e003688779d3212bf87261cfc747cab96b48
Adarsh rawat <00adarshrawat00@gmail.com>
2023-06-30 17:37:00 +0530

    first Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Header = () => {
+  return <nav>
+
+    <h2>Adarsh Rawat</h2>
+    <div>
+      <a href='#home'>Home</a>
+      <a href='#work'>work</a>
+      <a href='#timeline'>Experience</a>
+      <a href='#testimonial'>Testimonial</a>
+      <a href='#contact'>Contact</a>
+    </div>
+    <a href='mailto:00adarshrawat00@gmail.com'>
+      <button>Email</button>
+    </a>
+
+
+  </nav>;
+};
+
+export default Header
---
+++ src/components/Home.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { motion } from "framer-motion"
+import Typewriter from 'typewriter-effect';
+import { BsArrowUpRight } from "react-icons/bs"
+// import me from "../assets/me.jpg"
+
+const Home = () => {
+
+  const animation = {
+    h1: {
+      initial: {
+        x: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        x: 0,
+        opacity: 1,
+      }
+    },
+    button: {
+      initial: {
+        y: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        y: 0,
+        opacity: 1,
+      }
+    }
+  }
+
+  return (
+    <div id='home'>
+      <section>
+        <div>
+          <motion.h1 {...animation.h1}>
+            Hi, I am <br /> Adarsh Rawat
+          </motion.h1>
+
+          <Typewriter options={{
+            strings: ["A Developer", "A Designer", "A Creater"],
+            autoStart: true,
+            loop: true,
+            wrapperClassName: "typewriterpara",
+            cursor: "",
+          }} />
+
+          <div>
+            <a href="mailto:00adarshrawat00@gmail.com"> Hire Me </a>
+            <a href="work"> Projects <BsArrowUpRight /> </a>
+          </div>
+
+          <article>
+            <p>
+              +<span>100</span>
+            </p>
+            <span>Client WorldWide</span>
+          </article>
+
+          <aside>
+            <article>
+              <p>
+                +<span>500</span>
+              </p>
+              <span>Projects Made</span>
+            </article>
+          
+            <article data-special>
+              <p>Contact</p>
+              <span>00adarshrawat00@gmail.com</span>
+            </article>
+          </aside>
+        </div>
+      </section>
+      <section>
+        {/* <img src={me} alt="Adarsh" /> */}
+      </section>
+    </div>
+  )
+}
+
+export default Home
