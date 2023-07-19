69416b50b793e7a8cf830efec86346321b5663e0
Adarsh rawat <00adarshrawat00@gmail.com>
2023-07-19 13:44:38 +0530

    responsive home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,12 @@ import {AiOutlineMenu} from "react-icons/ai"
 const Header = ({menuOpen, setMenuOpen}) => {
   return <nav>
 
-    <h2>Adarsh Rawat</h2>
+    <h2>
+      <span className='green'>A</span>
+      <span>darsh</span>
+      <span className='green'> R</span>
+      <span>awat</span>
+    </h2>
     <div>
       <a href='#home'>Home</a>
       <a href='#work'>Work</a>
@@ -13,9 +18,15 @@ const Header = ({menuOpen, setMenuOpen}) => {
       <a href='#testimonial'>Testimonial</a>
       <a href='#contact'>Contact</a>
     </div>
+    <div>
     <a href='mailto:00adarshrawat00@gmail.com'>
       <button>Email</button>
     </a>
+    <a href='https://docs.google.com/document/d/15lQh1WiPPCjSkPqjveS0EFdFxLkyEtVPrQ-ZMoqZ2NE/edit'>
+      <button>Resume</button>
+    </a>
+    </div>
+    
 
     <button onClick={()=>setMenuOpen(!menuOpen)}>
       <AiOutlineMenu/>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,27 +2,11 @@ import React, { useRef } from 'react';
 import { animate, motion } from "framer-motion"
 import Typewriter from 'typewriter-effect';
 import { BsArrowUpRight, BsChevronDown } from "react-icons/bs"
-import me from "../assets/me.jpeg"
+import me from "../assets/me.png"
 import "../styles/home.scss"
 import "../styles/mediaquery.scss"
 
 const Home = () => {
-
-  const clientCount = useRef(null);
-  const projectCount = useRef(null);
-  const animationClientsCount = ()=>{
-    animate(0, 100,{
-      duration:1,
-      onUpdate:(v)=>(clientCount.current.textContent = v.toFixed()),
-    })
-  }
-
-  const animationProjectsCount = ()=>{
-    animate(0, 200, {
-      duration:1,
-      onUpdate:(v)=>(projectCount.current.textContent = v.toFixed()),
-    })
-  }
 
   const animation = {
     h1: {
@@ -51,53 +35,29 @@ const Home = () => {
     <div id='home'>
       <section>
         <div>
-          <motion.h1 {...animation.h1}>
-            Hi, I am <br /> Adarsh Rawat
-          </motion.h1>
+          <div className="left">
+            <motion.h1 {...animation.h1}>
+              Hi, I am <br /> Adarsh Rawat
+            </motion.h1>
 
-          <Typewriter options={{
-            strings: ["A Developer", "A Designer", "A Creater"],
-            autoStart: true,
-            loop: true,
-            wrapperClassName: "typewriterpara",
-            cursor: "",
-          }} />
-
-          <div>
-            <a href="mailto:00adarshrawat00@gmail.com"> Hire Me </a>
-            <a href="work"> Projects <BsArrowUpRight /> </a>
+            <Typewriter options={{
+              strings: ["A Backend Developer", "A Fronted Designer", "A Devops Enthusiastic"],
+              autoStart: true,
+              loop: true,
+              wrapperClassName: "typewriterpara",
+              cursor: "",
+            }} />
+            <p>Fullstack Developer, 
+              Hi there! My name is Adarsh Rawat and I’m a software developer and having a knowledge of MERN technology with git and github, docker, ansible and terraform. I love all things tech and coding, and I love to share my knowledge with others.</p>
+            <button><a href='mailto:00adarshrawat00@gmail.com'> GitHup Profile 🎉</a></button>
           </div>
-
-          <article>
-            <p>
-              +<motion.span whileInView={animationClientsCount} ref={clientCount}>100</motion.span>
-            </p>
-            <span>Client WorldWide</span>
-          </article>
-
-          <aside>
-            <article>
-              <p>
-              +<motion.span whileInView={animationProjectsCount} ref={projectCount}>100</motion.span>
-              </p>
-              <span>Projects Made</span>
-            </article>
-
-            <article data-special>
-              <p>Contact</p>
-              <span>00adarshrawat00@gmail.com</span>
-            </article>
-          </aside>
+          <div className="right">
+            <img src={me} alt="My imgae" />
+          </div>
         </div>
-      </section>
-      <section>
-        <img src={me} alt="Adarsh" />
-      </section>
+      </section >
 
-
-      <BsChevronDown />
-      
-    </div>
+    </div >
   )
 }
 export default Home
